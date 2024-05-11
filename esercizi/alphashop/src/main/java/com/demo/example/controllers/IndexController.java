2835eb1da2b2c7e8cc741001f0b51f7fbce7bf9c
Roberto Gianotto <gianottoroberto@gmail.com>
2024-05-11 12:25:00 +0200

    Esempio di passaggio di parametro con RequestParam e teoria

--- esercizi/alphashop/src/main/java/com/demo/example/controllers/IndexController.java
+++ esercizi/alphashop/src/main/java/com/demo/example/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.demo.example.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -17,8 +18,8 @@ public class IndexController {
 	}
 	
 	@GetMapping(value = "/index")
-	public String getWelcome2(Model model) {
-		model.addAttribute("intestazione", "Benvenuti nella index page della webapp Alphashop");
+	public String getWelcome2(Model model, @RequestParam("name") String name) {
+		model.addAttribute("intestazione", String.format("Benvenuto %s nella index page della webapp Alphashop", name));
 		model.addAttribute("saluti", saluti);
 		return "index";
 	}
