13490bfefc0c24f96e49b1afbaef25e35783c36f
Roberto Gianotto <gianottoroberto@gmail.com>
2024-08-25 12:01:35 +0200

    aggiornamento della teoria con appunti JPA e modifiche

--- esercizi/alphashop/src/main/java/com/demo/example/Application.java
+++ esercizi/alphashop/src/main/java/com/demo/example/Application.java
@@ -1,9 +1,13 @@
 package com.demo.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
